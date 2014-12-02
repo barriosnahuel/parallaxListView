72b4bd6ae3677457897058fd4dea3086983cd981
Nahuel Barrios <barrios.nahuel@gmail.com>
2014-12-02 10:39:32 -0300

    Minor change.

--- app/src/main/java/com/gnod/parallaxlistview/ui/expandonscrollheader/listener/impl/ExpandOnScrollListenerImpl.java
+++ app/src/main/java/com/gnod/parallaxlistview/ui/expandonscrollheader/listener/impl/ExpandOnScrollListenerImpl.java
@@ -50,30 +50,6 @@ public class ExpandOnScrollListenerImpl implements ExpandOnScrollListener {
         this.resetMethod = resetMethod;
     }
 
-    public void onScrollChanged(int l, int t, int oldl, int oldt) {
-//        Log.v(TAG, "onScrollChanged...");
-//        View firstView = (View) imageView.getParent();
-//
-//        if (firstView == null) {
-//            Log.d(TAG, "Error!");
-//            firstView = viewPager;
-//        } else {
-////            Log.d(TAG, "imageViewParent id: " + firstView.getId());
-////            Log.d(TAG, "imageView id: " + imageView.getId());
-//        }
-//
-//        // firstView.getTop < getPaddingTop means imageView will be covered by top padding, so we can layout it to make it shorter
-//        if (firstView != null && firstView.getTop() < paddingTop && imageView.getHeight() > imageViewHeight) {
-//            Log.d(TAG, "This log message never appears... should I delete this method (entirely!)");//  TODO : Delete this method
-//
-//            imageView.getLayoutParams().height = Math.max(imageView.getHeight() - (paddingTop - firstView.getTop()), imageViewHeight);
-//
-//            // to set the firstView.mTop to 0, maybe use View.setTop() is more easy, but it just support from Android 3.0 (API 11)
-//            firstView.layout(firstView.getLeft(), 0, firstView.getRight(), firstView.getHeight());
-//            imageView.requestLayout();
-//        }
-    }
-
     @Override
     public boolean overScrollBy(int deltaX, int deltaY, int scrollX, int scrollY, int scrollRangeX, int scrollRangeY, int maxOverScrollX, int maxOverScrollY, boolean isTouchEvent) {
         Log.v(TAG, "overScrollBy...");
@@ -137,5 +113,29 @@ public class ExpandOnScrollListenerImpl implements ExpandOnScrollListener {
             animation.setDuration(ANIMATION_DURATION);
             viewToAnimate.startAnimation(animation);
         }
+    }
+
+    public void onScrollChanged(int l, int t, int oldl, int oldt) {
+//        Log.v(TAG, "onScrollChanged...");
+//        View firstView = (View) imageView.getParent();
+//
+//        if (firstView == null) {
+//            Log.d(TAG, "Error!");
+//            firstView = viewPager;
+//        } else {
+////            Log.d(TAG, "imageViewParent id: " + firstView.getId());
+////            Log.d(TAG, "imageView id: " + imageView.getId());
+//        }
+//
+//        // firstView.getTop < getPaddingTop means imageView will be covered by top padding, so we can layout it to make it shorter
+//        if (firstView != null && firstView.getTop() < paddingTop && imageView.getHeight() > imageViewHeight) {
+//            Log.d(TAG, "This log message never appears... should I delete this method (entirely!)");//  TODO : Delete this method
+//
+//            imageView.getLayoutParams().height = Math.max(imageView.getHeight() - (paddingTop - firstView.getTop()), imageViewHeight);
+//
+//            // to set the firstView.mTop to 0, maybe use View.setTop() is more easy, but it just support from Android 3.0 (API 11)
+//            firstView.layout(firstView.getLeft(), 0, firstView.getRight(), firstView.getHeight());
+//            imageView.requestLayout();
+//        }
     }
 }
